4221e1b680f8a169ced8e8044f5e1337bef3cfa3
冯波 <fengbo@idaddy.cn>
2019-03-19 15:09:03 +0800

    添加stringutil

---
+++ hello/hello_world.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rshark/golang_studying/stringutil"
+)
+
+func main()  {
+	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+}
