323d2b2405f8aecdad43d1682d4c73d551c8549e
冯波 <fengbo@idaddy.cn>
2019-03-25 15:52:16 +0800

    添加pkg

---
+++ pkg/encoding/json/json.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+func main() {
+	// intent() example
+	jsonIntentExample()
+
+	//
+}
+
+func jsonIntentExample() {
+	type Road struct {
+		Name   string
+		Number int
+	}
+
+	roads := []Road{
+		{"Diamond Fork", 29},
+		{"Sheep Creek", 51},
+	}
+
+	b, err := json.Marshal(roads)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var out bytes.Buffer
+	json.Indent(&out, b, "=", "\t")
+	out.WriteTo(os.Stdout)
+}
